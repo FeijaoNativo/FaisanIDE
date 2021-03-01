5316be867da0c2a9150dab0ba5694fdd81ab230b
Samuel Ives <sivesssl@gmail.com>
2021-03-01 10:27:52 -0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <QApplication>
+
+#include "ui/mainwindow.hpp"
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    MainWindow w;
+    w.show();
+
+    return app.exec();
+}
---
+++ src/ui/mainwindow.hpp
@@ -0,0 +1,24 @@
+#ifndef MAINWINDOW_HPP
+#define MAINWINDOW_HPP
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+
+    Ui::MainWindow *ui = nullptr;
+};
+
+#endif // MAINWINDOW_HPP
